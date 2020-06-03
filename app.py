efad89c2e464d4bcf6dffa4a76cf9a1224d831b6
Diana Arreola <arreoladiana11@gmail.com>
2020-06-03 11:23:43 -0700

    added homepage, signin page, signup page,and dashboard page

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+
+
