2e8a7be64bd142dafc14859de0b159a61180a026
Diana Arreola <arreoladiana11@gmail.com>
2020-06-04 12:17:24 -0700

    registered users added to the datatbase

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,8 @@ from fastapi.staticfiles import StaticFiles
 from db import models, crud
 from db.database import SessionLocal, engine
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+import logging
 
 app = FastAPI()
 
@@ -18,7 +20,7 @@ templates = Jinja2Templates(directory="templates")
 models.Base.metadata.create_all(bind=engine)
 
 # Dependency
-def get_db():
+def get_db(): 
     db = SessionLocal()
     try:
         yield db
@@ -38,13 +40,17 @@ def login(request: Request):
 def login(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
-@app.post("/users/create", response_model=User)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    user = crud.create_user(db=db, user=user)
+logger = logging.getLogger("api")
+# # /api  -click part
+@app.post("/users", response_model=User)
+def create_user(request: Request, user: UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return templates.TemplateResponse("dashboard.html", {"request": request, "user":user})
+    user = crud.create_user(db=db, user=user)
+    logger.debug("in here")
+    return templates.TemplateResponse("users.html", {"request": request, "user":user})
+    
 
 # @app.get("/dashboard/<int:user_id>'")
 # def dashboard(request: Request):
@@ -57,11 +63,11 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 
 @app.get("/users/{user_id}", response_model=User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(request: Request, user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return templates.TemplateResponse("users.html", {"request": request, "user_id":user_id})
 
 
 @app.post("/test/", response_model=Order)
