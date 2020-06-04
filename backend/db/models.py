2cd44532a8d97d15e82c254337d19b294df51e8f
Rachel Liu <rachel98rl@hotmail.com>
2020-06-03 23:47:32 -0400

    Add click component

---
+++ backend/db/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    is_admin = Column(Boolean, default=False)
+    password = Column(String)
+
+    orders = relationship("Order", backref="users")
+
+
+class Order(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(String, unique=True, index=True)  # Platform order id
+    platform_id = Column(Integer, ForeignKey("platforms.id"))
+    status = Column(Integer, default=0)
+    user_email = Column(String, ForeignKey("users.email"))
+
+
+class Platform(Base):
+    __tablename__ = "platforms"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+    orders = relationship("Order", backref="platforms")
