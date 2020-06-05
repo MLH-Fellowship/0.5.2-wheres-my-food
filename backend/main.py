01235638ccfb4f96fa3b36605a495c01dc4de240
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-06-04 22:39:12 -0500

    Added frontend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 import uvicorn
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Request, Response, Cookie
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from db.schemas.user_schemas import User, UserCreate
 from db.schemas.order_schemas import Order, OrderCreate
@@ -14,9 +17,15 @@ from auth.auth import get_current_active_user, get_admin_user
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+origins = ["http://localhost", "http://localhost:8000", "http://localhost:5000", "*"]
 
-templates = Jinja2Templates(directory="templates")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -30,29 +39,22 @@ def get_db():
 
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get("/login")
-def login(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
-@app.get("/register")
-def login(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-
-@app.get("/dashboard")
-def login(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+async def health_check():
+    return {"status": "cool 😎"}
 
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
 ):
+    import logging
+
+    logger = logging.getLogger("api")
+    logger.debug(form_data)
+    print(form_data.username)
+    print(form_data.password)
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -62,13 +64,14 @@ async def login_for_access_token(
         )
 
     access_token = create_access_token(data={"sub": user.email})
-
+    response.set_cookie(key="wmf_cookie", value=access_token)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get("/me", response_model=User)
 async def get_me(
-    current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)
+    current_user: User = Depends(get_current_active_user),
+    db: Session = Depends(get_db),
 ):
     db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
@@ -85,13 +88,7 @@ def create_user(
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    user = crud.create_user(db=db, user=user)
-    return user
-
-
-# @app.get("/dashboard/<int:user_id>'")
-# def dashboard(request: Request):
-#     return templates.TemplateResponse("register.html", {"request": request})
+    return crud.create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=List[User])
@@ -114,14 +111,7 @@ def read_user(
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return templates.TemplateResponse(
-        "users.html", {"request": request, "user_id": user_id}
-    )
-
-
-@app.post("/test/", response_model=Order)
-def create_order_for_user(order: OrderCreate, db: Session = Depends(get_db)):
-    return {"message": "Hi from post"}
+    return db_user
 
 
 @app.post("/orders/", response_model=Order)
